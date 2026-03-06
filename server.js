0beb1569d7a41c553c018a6ed6c622bd69f65427
rajatwarude <165987108+rajatwarude@users.noreply.github.com>
2026-03-06 23:16:56 +0530

    fix render port issue

--- server.js
+++ server.js
@@ -41,9 +41,11 @@ app.get("/protected", verifyToken, (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("MongoDB Connected");
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 })
 .catch(err => console.log(err));
-
